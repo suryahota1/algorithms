e77f16f7e976575736dbf5444e94f64148348850
SuryaHota <tech.suryakanthota@gmail.com>
2023-03-21 21:08:19 +0530

    Binomial tree implementation added

---
+++ js/heap/BinomialTree.js
@@ -0,0 +1,45 @@
+/* 
+    1. A binaomial heap is based on a binomial tree data structure.
+    2. A binomial tree of order k consists of two binomial trees of order k - 1 and this holds 
+    recursively true for its child components.
+    3. While forming a binomial tree of order k, we have to recursively break it into two 
+    binomial trees of order k - 1 and make one as left most child of the other one.
+    4. There are exactly 2 ^ k nodes for a binomial tree of order k.
+    5. The height of tree is k.
+    6. There are exactly k C i nodes at height i of a k order binomial tree.
+    7. For any binomial tree Bk, its children are trees of order k-1, k-2, k-3, ..., 1, 0 
+    from left to right.
+*/
+
+class Node {
+    
+    data;
+    children;
+
+    constructor ( data ) {
+        this.data = data;
+        this.children = [];
+    }
+}
+
+class BinomialTree1 {
+
+    root;
+    order;
+
+    constructor ( order ) {
+        this.root = null;
+        this.order = order;
+    }
+
+    async #createTree ( k ) {
+        if ( k === 0 ) return new Node(Math.ceil(Math.random() * 100));
+        const root1 = await this.#createTree(k - 1);
+        const root2 = await this.#createTree(k - 1);
+        root1.children.unshift(root2);
+    }
+
+    async constructTree () {
+        return await this.#createTree(this.order);
+    }
+}
--- js/heap/MinHeap1.js
+++ js/heap/MinHeap1.js
@@ -23,7 +23,7 @@ class MinHeap1 {
     }
 
     #moveElementToTop ( idx ) {
-        while ( idx >= 0 ) {
+        while ( idx > 0 ) {
             let parentIdx = this.getParentIdx();
             if ( parentIdx <= 0 || this.heap[parentIdx] <= this.heap[idx] ) return;
             let temp = this.heap[parentIdx];
@@ -55,12 +55,27 @@ class MinHeap1 {
 
     insert ( value ) {
         if ( this.heapSize === this.maxSize ) throw new Error("Max heap limit reached");
-        let idx = this.heapSize;
         this.heap[this.heapSize++] = value;
         this.#moveElementToTop();
     }
 
-    delete ( idx ) {
+    #swap ( idx1, idx2 ) {
+        let temp = this.heap[idx1];
+        this.heap[idx1] = this.heap[idx2];
+        this.heap[idx2] = temp;
+    }
 
+    delete ( idx ) {
+        if ( idx < 0 || idx >= this.heapSize ) throw new Error("Invalid index");
+        if ( this.heapSize === 0 ) throw new Error("Empty heap");
+        if ( this.heapSize === 1 ) return this.heap[this.heapSize--];
+        if ( idx === 0 ) {
+            this.#swap(0, this.heapSize-- - 1);
+            this.minHeapify(0);
+        } else {
+            this.heap[idx] = Number.MIN_VALUE;
+            this.#moveElementToTop(idx);
+            this.delete(0);
+        }
     }
 }
