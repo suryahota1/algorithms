fe6f61625caa964ac18f8bdc2c1d6fd69b2168dc
SuryaHota <tech.suryakanthota@gmail.com>
2024-01-25 22:56:31 +0530

    get longest common prefix

--- js/trie/Trie.js
+++ js/trie/Trie.js
@@ -24,11 +24,11 @@ class Trie {
         let root = this.root;
         for ( let i = 0; i < word.length; i++ ) {
             const idx = word.charCodeAt(i) - 97;
-            let nextRoot = root[idx];
+            let nextRoot = root.children[idx];
             if ( !nextRoot ) {
                 console.log("creating next root for ", word[i]);
                 nextRoot = new TrieNode();
-                root[idx] = nextRoot;
+                root.children[idx] = nextRoot;
             }
             nextRoot.prefixCount++;
             root = nextRoot;
@@ -41,8 +41,8 @@ class Trie {
         let root = this.root;
         for ( let i = 0; i < key.length; i++ ) {
             const idx = key.charCodeAt(i) - 97;
-            if ( !root[idx] ) return false;
-            root = root[idx];
+            if ( !root.children[idx] ) return false;
+            root = root.children[idx];
         }
         return root.prefixCount > 0;
     }
@@ -51,8 +51,8 @@ class Trie {
         let root = this.root;
         for ( let i = 0; i < word.length; i++ ) {
             const idx = word.charCodeAt(i) - 97;
-            if ( !root[idx] ) return false;
-            root = root[idx];
+            if ( !root.children[idx] ) return false;
+            root = root.children[idx];
         }
         return root.wordEndCount > 0;
     }
@@ -64,8 +64,8 @@ class Trie {
         if ( keyIdx < word.length ) {
             // Reduce curr prefixCount if deleted
             const idx = word.charCodeAt(keyIdx) - 97;
-            const nextNode = this.#deleteCurrWord(root[idx], word, keyIdx + 1);
-            root[idx] = nextNode;
+            const nextNode = this.#deleteCurrWord(root.children[idx], word, keyIdx + 1);
+            root.children[idx] = nextNode;
             if ( nextNode === null ) {
                 root.prefixCount--;
                 if ( root.wordEndCount === 0 && root.prefixCount === 0 ) {
@@ -88,26 +88,56 @@ class Trie {
     deleteWord ( word ) {
         return this.#deleteCurrWord(this.root, word, 0);
     }
-}
 
+    #getOnlyOccuringChar ( root ) {
+        let idx;
+        for ( let i = 0; i < root.children.length; i++ ) {
+            if ( root.children[i]?.prefixCount > 0 ) {
+                if ( idx !== undefined ) return undefined;
+                else idx = i;
+            }
+        }
+        return idx;
+    }
+
+    getLongestCommonPrefix () {
+        let root = this.root, str = "";
+        while ( root ) {
+            const idx = this.#getOnlyOccuringChar(root);
+            if ( idx === undefined ) return str;
+            str += String.fromCharCode(idx + 97);
+            root = root.children[idx];
+        }
+        return str;
+    }
+}
 
 try {
     const trObj = new Trie();
-    trObj.insert("and");
-    trObj.insert("ant");
-    console.log("tri contains prefix and", trObj.searchWord("and"));
-    trObj.deleteWord("and");
-    console.log("tri contains prefix and", trObj.searchWord("and"));
-    trObj.insert("and");
-    trObj.insert("an");
-    trObj.deleteWord("ant");
-    trObj.deleteWord("and");
-    trObj.deleteWord("an");
-    console.log("tri contains prefix and", trObj.searchWord("ant"));
-    console.log("tri contains prefix and", trObj.searchWord("and"));
-    console.log("tri contains prefix and", trObj.searchWord("an"));
-    trObj.insert("and");
-    console.log("tri contains prefix and", trObj.searchWord("and"));
+    // trObj.insert("and");
+    // trObj.insert("ant");
+    // console.log("tri contains prefix and", trObj.searchWord("and"));
+    // trObj.deleteWord("and");
+    // console.log("tri contains prefix and", trObj.searchWord("and"));
+    // trObj.insert("and");
+    // trObj.insert("an");
+    // trObj.deleteWord("ant");
+    // trObj.deleteWord("and");
+    // trObj.deleteWord("an");
+    // console.log("tri contains prefix and", trObj.searchWord("ant"));
+    // console.log("tri contains prefix and", trObj.searchWord("and"));
+    // console.log("tri contains prefix and", trObj.searchWord("an"));
+    // trObj.insert("and");
+    // console.log("tri contains prefix and", trObj.searchWord("and"));
+
+    trObj.insert("geeksforgeeks");
+    trObj.insert("geeks");
+    trObj.insert("geek");
+    trObj.insert("geezer");
+
+    console.log("tri contains prefix and", trObj.searchWord("april"));
+
+    console.log("getLongestCommonPrefix", trObj.getLongestCommonPrefix());
 } catch ( e ) {
     console.log("Error in Trie", e);
 }
